93b74554d6bb20398c90933b2b61d015576ad5cc
liuzhiyang <zhiyang1.liu@ximalaya.com>
2021-12-03 18:25:05 +0800

    兼容commonjs打包和打包node代码

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import rollupTypescript from 'rollup-plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 import image from '@rollup/plugin-image';
-
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+// import globals from 'rollup-plugin-node-globals'; // 加global前缀
+// import builtins from 'rollup-plugin-node-builtins';
+import nodeExternals from 'rollup-plugin-node-externals';
 export default {
     input: 'src/index.ts',
-    external(id) {
-        return id === 'react';
-    },
+    external:['react'],
     output: {
         file: 'lib/index.js',
         format: 'cjs',
@@ -15,8 +17,13 @@ export default {
         }
     },
     plugins: [
+        nodeExternals(),
         rollupTypescript(),
         image(),
         postcss(),
+        nodeResolve({ preferBuiltins: false }), // or `true`
+        commonjs(),
+        // globals(),
+        // builtins(),
     ],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-export * from './tools/createFakeWindow';
+import { Listr } from 'listr2'
+import {execa} from '@marchyang/execa'
+import path from 'path';
+interface Ctx {
+    /* some variables for internal use */
+}
+
+const tasks = new Listr<Ctx>(
+    [
+        {
+            title: 'Install dependent packages',
+            task:() => {
+                return execa('npm',['install', 
+                    'eslint',
+                    'babel-eslint', 
+                    '@typescript-eslint/eslint-plugin',
+                    '@typescript-eslint/parser',
+                    'eslint-plugin-react',
+                    'eslint-plugin-react-hooks',
+                    'jsx-control-statements'
+                ])
+            }
+        },
+        {
+            title: 'copy .eslintrc file',
+            task:() => {
+                return execa('cp',[path.resolve(__dirname,'../.eslintrc.js'), process.cwd()])
+            }
+        }
+    ],
+    {
+        /* options */
+    }
+)
+
+
+try {
+    tasks.run()
+} catch (e) {
+    console.error(e)
+}
